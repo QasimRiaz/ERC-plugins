d3bb0133d02ef4d42a27a0af9a0df60f84fc1dac
QasimRiaz <qasim.riaz@e2esp.com>
2015-01-09 15:03:01 +0500

    sale -force -mappaing

---
+++ sf-mapping-srcdemo-staging/sf_mapping_srcdemo_staging_setup.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+  Plugin Name: Salesforce Mapping with User Meta 
+  Description: Saleforce Fields Map with User meta fields.
+  Author: Qasim Riaz
+ 
+ */
+
+
+
+add_action('admin_menu', 'register_my_saleforce_mapping');
+// example custom dashboard widget
+function register_my_saleforce_mapping() {
+    add_menu_page('Salesforce Mapping', 'Salesforce Mapping', 'manage_options', 'my-custom-saleforce-mapping', 'my_saleforce_mapaing');
+}
+function my_saleforce_mapaing(){
+    
+   delete_option( "sf_settings_client_keys" );
+   delete_option( "sf_um_contact_keys" );
+   delete_option( "sf_um_account_keys" );
+   $sf_contact= array(
+        "user_login_name__c" =>"nickname",
+        "LastName" =>"last_name",
+        "FirstName" =>"first_name"
+    );
+   
+    $sf_account= array(
+        "Name"=>"Company_Name"
+        
+//        "task_add_twitter_handle__c" =>"task_add_twitter_handle",
+//        "task_add_twitter_handle_status__c" =>"task_add_twitter_handle_status",
+//        "task_company_name__c" =>"task_company_name",
+//        "task_company_name_status__c" =>"task_company_name_status"
+    );
+    
+    $sf_settings_keys = array(
+        "client_id" =>"3MVG9Y6d_Btp4xp5.SIezjlmUZm6xC7wOpmRB.NLlOEiWm7yaW4L_aC8SNxtQ87OhSOcGGe3uVu2WiEixwDK3",
+        "client_secret" =>"4887576683398102777",
+        "redirect_uri" =>"https://srcdemo.staging.wpengine.com/wp-content/plugins/active-saleforce-account/oAth-responce.php",
+        "login_url" =>"https://login.salesforce.com"
+        );
+    
+     add_option("sf_settings_client_keys",$sf_settings_keys,"test");    
+     add_option("sf_um_contact_keys",$sf_contact,"test");
+     add_option("sf_um_account_keys",$sf_account,"test");
+     $salesforcekeys_contact=get_option("sf_um_contact_keys");
+     $salesforcekeys_account=get_option("sf_um_account_keys");
+     $salesforcesettingskeys = get_option("sf_settings_client_keys"); 
+     $output = '<h1>Salesforce Mapping with User Meta Keys</h1><table class="mytable"> <tr><td><h3>Salesforce Key</h3></td><td><h3>User Meta Key</h3></td></tr>';
+    
+     foreach ($salesforcekeys_contact as $salesforcekeys_name => $salesforcekeys_value) {
+         $output .= '<tr><td>'.$salesforcekeys_name.'</td><td>'.$salesforcekeys_value.'</td></tr>';
+     }
+     
+     foreach ($salesforcekeys_account as $salesforce_name => $salesforce_value) {
+         $output .= '<tr><td>'.$salesforce_name.'</td><td>'.$salesforce_value.'</td></tr>';
+     }
+     
+     $output.='</table>';
+      
+     echo $output;
+      
+     $clientkeys = '<h1>Salesforce Account Setting Keys</h1> <table  class="mytable"><tr>
+      <td>Client ID </td><td>'.$salesforcesettingskeys['client_id'].'</td></tr>
+      <tr><td>Client Secret</td><td>'.$salesforcesettingskeys['client_secret'].'</td></tr>
+      <tr><td>Redirect Url</td><td>'.$salesforcesettingskeys['redirect_uri'].'</td></tr>
+      </table>';
+      echo $clientkeys;
+ }
+ 
+?>
+
